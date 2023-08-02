54feab8c9a0a077157cf215e432313b548073706
obuzietter <obuyacalvince672@gmail.com>
2023-08-02 16:26:32 +0300

    project start

---
+++ resources/views/home.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="{{./styles/home.css}}">
+    <title>HOME - WHITE SHADOW</title>
+</head>
+<body>
+    <nav>
+        <div class="logo">
+            <span class="logo-img">
+                Image
+            </span>
+            <span class="logo-name">
+                White Shadow
+            </span>
+        </div>
+        <div class="links">
+            <span>ABOUT</span>
+            <span>HOME</span>
+            <span>GALLERY</span>
+            <span>CONTACT</span>
+        </div>
+        <div class="socials">
+            <span class="instagram">
+                instagram - image
+            </span>
+            <span class="whatsapp">
+                whatsapp - image
+            </span>
+        </div>
+    </nav>
+    <main>
+        <div>OUR WORK</div>
+        <div>
+            <p>~</p>
+            <h1>WHITE SHADOW PHOTOGRAPHY</h1>
+            <p>~</p>
+        </div>
+        <div>HOW TO FIND US</div>
+    </main>
+    <footer>
+        &copy; 2023 by white shadow | All rights reserved.
+    </footer>
+</body>
+</html>
