19b68643e6fb7f974db422094cabbff511546279
obuzietter <obuyacalvince672@gmail.com>
2023-08-02 18:16:51 +0300

    problem with socials

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,49 +1,65 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link rel="stylesheet" href="{{./styles/home.css}}">
+    <link rel="stylesheet" href="{{ url('/styles/home.css') }}">
+    <script src="https://kit.fontawesome.com/5ce449abc0.js" crossorigin="anonymous"></script>
+
     <title>HOME - WHITE SHADOW</title>
 </head>
+
 <body>
     <nav>
         <div class="logo">
             <span class="logo-img">
-                Image
+                <img src="https://cdn-icons-png.flaticon.com/128/2983/2983067.png" alt="">
             </span>
             <span class="logo-name">
                 White Shadow
             </span>
         </div>
         <div class="links">
-            <span>ABOUT</span>
-            <span>HOME</span>
-            <span>GALLERY</span>
-            <span>CONTACT</span>
+            <span><a href="">ABOUT</a></span>
+            <span><a href="">HOME</a></span>
+            <span><a href="">GALLERY</a></span>
+            <span><a href="">CONTACT</a></span>
         </div>
         <div class="socials">
-            <span class="instagram">
-                instagram - image
+            <i class="instagram">
+                <img src="https://cdn-icons-png.flaticon.com/128/3955/3955024.png" alt="">
             </span>
-            <span class="whatsapp">
-                whatsapp - image
+            <i class="whatsapp">
+                {{-- <img src="https://cdn-icons-png.flaticon.com/128/3670/3670051.png" alt=""> --}}
             </span>
         </div>
     </nav>
     <main>
-        <div>OUR WORK</div>
         <div>
-            <p>~</p>
-            <h1>WHITE SHADOW PHOTOGRAPHY</h1>
-            <p>~</p>
+            <div class="gallery">
+                OUR WORK
+            </div>
         </div>
-        <div>HOW TO FIND US</div>
+        <div>
+            <div class="banner">
+                <p>~</p>
+                <h5>WHITE SHADOW PHOTOGRAPHY</h5>
+                <p>~</p>
+            </div>
+
+        </div>
+        <div>
+            <div class="contact">
+                HOW TO FIND US
+            </div>
+        </div>
     </main>
     <footer>
         &copy; 2023 by white shadow | All rights reserved.
     </footer>
 </body>
+
 </html>
